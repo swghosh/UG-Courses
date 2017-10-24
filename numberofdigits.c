9b657dbc1002b4622ae0a8255cd7f314eb6f7c71
SwG Ghosh <codecrafts.cf@icloud.com>
2017-10-24 14:02:17 +0530

    Added comments to last two loop programs

--- numberofdigits.c
+++ numberofdigits.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 
 int main() {
+	// initialisation
 	int number, digits;
 	digits = 0;
+	// take user input
 	printf("Enter an integer: ");
 	scanf("%d", &number);
+	// calculate number of digits by repetitive division
 	for(;number > 0; number = number / 10) digits++;
+	// print number of digits
 	printf("Number of digits is %d.\n", digits);
 	return 0;
 }
--- sumtillzero.c
+++ sumtillzero.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
 int main() {
+	// initialisation
 	int num, sum;
+	// keep adding sum from user input
+	// and till user inputs zero
 	for(sum = 0; num != 0; ) {
 		printf("Enter a number: (integer) -> ");
 		scanf("%d", &num);
 		sum = sum + num;
 	}
+	// print the final sum
 	printf("Sum: %d.\n", sum);
 	return 0;
 }
