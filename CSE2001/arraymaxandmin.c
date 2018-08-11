73d6b0037ca04a64c4d0277d8f6d311df42a5a10
SwG Ghosh <codecrafts.cf@icloud.com>
2018-08-11 20:39:26 +0530

    Added arraymaxmin in CSE2001

---
+++ CSE2001/arraymaxandmin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
+#define MAX_ELEMENTS 100
+
+int main() {
+    int numbers[MAX_ELEMENTS], userLength, iterIndex;
+    int max = INT_MIN, min = INT_MAX;
+    printf("How many numbers? (integer < 100, expected) > ");
+    scanf("%d", &userLength);
+
+    printf("Specify all numbers. (space seperated, %d integers expected) > ", userLength);
+    for(iterIndex = 0; iterIndex < userLength; iterIndex++) {
+        scanf("%d", &numbers[iterIndex]);
+
+        if(numbers[iterIndex] > max) max = numbers[iterIndex];
+        if(numbers[iterIndex] < min) min = numbers[iterIndex];
+    }
+
+    printf("Maximum and minimum of all the elements are %d and %d respectively. \n", max, min);
+    return 0;
+}
