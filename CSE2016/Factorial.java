5afd1b219d26187f92c0123c6d7f430e9196a885
SwG Ghosh <codecrafts.cf@icloud.com>
2018-08-14 12:14:00 +0530

    Added few documentation

--- CSE2016/Factorial.java
+++ CSE2016/Factorial.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * Simple program to find the factorial of
+ * user input number using the recursion technique
+ * 
+ * @author Swarup Ghosh
+ * @since 2018-08-14
+ * @version 1.0
+ * 
+ */
+
 class Factorial {
 
+    /**
+     * Method to find the factorial of an integer 
+     * using recursion technique
+     * @param number integer whose factorial is to be calculate
+     * @return factorial of number
+     */
     public static long factorial(int number) {
         if(number <= 1) {
             return 1;
@@ -10,7 +26,13 @@ class Factorial {
             return number * factorial(number - 1);
         }
     }
-
+    
+    /** 
+     * Main method that is to be executed at runtime,
+     * will take a single user input from stdin,
+     * and print its factorial
+     * @param args command line arguments (no arguments necessary)
+     */
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.print("Number (integer expected) -> ");
--- CSE2016/SumOfDigits.java
+++ CSE2016/SumOfDigits.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 
+/**
+ * Simple program to find the sum of digits of
+ * user input number
+ * 
+ * @author Swarup Ghosh
+ * @since 2018-08-14
+ * @version 1.0
+ * 
+ */
+
 class SumOfDigits {
 
+    /**
+     * Method to find the sum of digits of an integer
+     * @param number integer whose sum of digits is to be found
+     * @return sum of digits
+     */
     public static long sumOfDigits(int number) {
         long sum = 0;
         for(; number > 0; number = number / 10) {
@@ -10,6 +25,12 @@ class SumOfDigits {
         return sum;
     }
 
+    /** 
+     * Main method that is to be executed at runtime,
+     * will take a single user input from stdin,
+     * and print its sum of digits
+     * @param args command line arguments (no arguments necessary)
+     */
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.print("Number (integer expected) -> ");
