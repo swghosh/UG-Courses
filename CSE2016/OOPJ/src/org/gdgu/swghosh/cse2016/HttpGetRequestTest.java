29e9dd6f197bc1d06a48b7c2fe23ecbea91aa0fc
SwG Ghosh <codecrafts.cf@icloud.com>
2018-12-05 20:28:48 +0530

    Added HTTPS support to URL connect test

--- CSE2016/OOPJ/src/org/gdgu/swghosh/cse2016/HttpGetRequestTest.java
+++ CSE2016/OOPJ/src/org/gdgu/swghosh/cse2016/HttpGetRequestTest.java
@@ -3,9 +3,7 @@ package org.gdgu.swghosh.cse2016;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Scanner;
 
 public class HttpGetRequestTest {
@@ -13,26 +11,31 @@ public class HttpGetRequestTest {
         Scanner in = new Scanner(System.in);
         System.out.print("Specify an URL (http[s] URL expected) -> ");
         URL userUrl = new URL(in.nextLine());
-        // case of http
-        if(userUrl.getProtocol().equals("http")) {
-            HttpURLConnection connection = (HttpURLConnection) userUrl.openConnection();
-            connection.setRequestMethod("GET");
-            // request text from the url (mime type: text/plain)
-            connection.setRequestProperty("Content-Type", "text/plain");
-            BufferedReader out = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()
-                    )
-            );
-            String line;
-            while((line = out.readLine()) != null) {
-                System.out.println(line);
-            }
-            out.close();
+        HttpURLConnection connection;
+        switch (userUrl.getProtocol()) {
+            case "http":
+                connection = (HttpURLConnection) userUrl.openConnection();
+                break;
+            case "https":
+                connection = (HttpsURLConnection) userUrl.openConnection();
+                break;
+            default:
+                System.err.println("Invalid URL protocol.");
+                return;
         }
-        else {
-            System.err.println("Invalid URL protocol.");
+        connection.setRequestMethod("GET");
+        // request text from the url (mime type: text/plain)
+        connection.setRequestProperty("Content-Type", "text/plain");
+        BufferedReader out = new BufferedReader(
+                new InputStreamReader(
+                        connection.getInputStream()
+                )
+        );
+        String line;
+        while((line = out.readLine()) != null) {
+            System.out.println(line);
         }
+        out.close();
         in.close();
     }
 }
