80e0409f778b40ba7170d372954b576d0d148695
SwG Ghosh <codecrafts.cf@icloud.com>
2019-02-27 01:51:31 +0530

    Modified HelloWorldServlet in osms-api to use Google JSON Library

--- CSE2017/osms-api/src/main/java/org/gdgu/swghosh/cse2017/osms/api/HelloWorldServlet.java
+++ CSE2017/osms-api/src/main/java/org/gdgu/swghosh/cse2017/osms/api/HelloWorldServlet.java
@@ -4,15 +4,18 @@ import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import com.google.gson.*;
 
-@WebServlet("/api")
+@WebServlet("/api/*")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException {
+        Gson gson = new Gson();
+        String jsonRepr = gson.toJson(new Test());
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
-        out.println("{\"message\": \"Hello World!\"}");
+        out.println(jsonRepr);
         out.close();
     }
 }
