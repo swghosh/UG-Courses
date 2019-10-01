7338ac25cc0cf416f87c8388a8125050bb02bc6b
SwG Ghosh <codecrafts.cf@icloud.com>
2019-10-02 03:04:18 +0530

    Added LRU page replacement in CSE3007

---
+++ CSE3005/DAA/Heap Sort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Heap Sort
+//
+//  Created by Swarup Ghosh on 26/09/19.
+//  Copyright © 2019 Swarup Ghosh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
