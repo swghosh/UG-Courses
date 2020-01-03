6e84b717be07d9d45223da9a9c1bb755514e4309
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-01-04 03:18:45 +0530

    Minor additions to CSE2721 project

---
+++ CSE3005/DAA/Kruskals Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Kruskals Algorithm
+//
+//  Created by Swarup Ghosh on 26/11/19.
+//  Copyright © 2019 Swarup Ghosh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
