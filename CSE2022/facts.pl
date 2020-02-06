99f3487e01296c896454b63fd42d2d01f091f119
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-02-06 10:49:49 +0530

    Add some files to CSE2022

---
+++ CSE2022/facts.pl
@@ -0,0 +1,4 @@
+% car(Person, Color): person Person has a car of Color color.
+car(john, white).
+car(luna, red).
+car(ben, black).
--- CSE2022/some_more_facts.pl
+++ CSE2022/some_more_facts.pl
@@ -1,3 +1,8 @@
+% car(Person, Color): person Person has a car of Color color.
+car(john, white).
+car(luna, red).
+car(ben, black).
+
 % downtown(X): place Downtown exists in in city X.
 downtown(manhattan, newyork).
 downtown(newtown, kolkata).
@@ -9,10 +14,16 @@ bigger(ant, fly).
 % is_bigger(X, Y): X is bigger than Y or not
 is_bigger(AnimalX, AnimalY) :- bigger(AnimalX, AnimalZ), bigger(AnimalZ, AnimalY).
 
-likes(john,mary).
-likes(john,trains).
-likes(peter,fast_cars). 
-likes(Person1,Person2):- hobby(Person1,Hobby), hobby(Person2,Hobby).
+% likes(X, Y): X is liked by Y
+likes(john, mary).
+likes(john, trains).
+likes(peter, fast_cars). 
+
+% Person1 like Person2 if they have a common hobby
+likes(Person1, Person2):- 
+    hobby(Person1 ,Hobby), hobby(Person2, Hobby).
+
+% hobby(X, Y): person X has hobby Y
 hobby(john,trainspotting). 
 hobby(tim,sailing). 
 hobby(helen,trainspotting). 
