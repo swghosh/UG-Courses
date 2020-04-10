0c6230ed1a87c0533f100ce3f182b0c1b120ef30
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-04-11 04:02:10 +0530

    Added tensor array replacing tf.Variable to fix tf.function

--- CSE2705/tf_kmeans.py
+++ CSE2705/tf_kmeans.py
@@ -4,34 +4,50 @@ from typeguard import typechecked
 from typing import Union
 
 @tf.function
-def train_kmeans(X: Union[tf.Tensor, np.ndarray], 
-    k: Union[int, tf.Tensor], 
-    n_iter: Union[int, tf.Tensor] = 10) -> (tf.Tensor, tf.Tensor):
+def kmeans(X: Union[tf.Tensor, np.ndarray], 
+    k: int, 
+    n_iter: int = 1) -> (tf.Tensor, tf.Tensor):
 
     X = tf.convert_to_tensor(X)
     X = tf.cast(X, tf.float32)
     assert len(tf.shape(X)) == 2, "Training data X must be represented as 2D array only"
-    m = tf.shape(X)[0]
-
-    k = tf.convert_to_tensor(k, dtype=tf.int64)
+    m, nd = X.get_shape().as_list()
 
     random_select = tf.random.shuffle(X)
     init_centroids = random_select[:k]
 
-    centroids = tf.Variable(init_centroids)
     clusters = tf.zeros([m, ], dtype=tf.int64)
-    clusters = tf.Variable(clusters)
+    centroids = init_centroids
+
     for _ in tf.range(n_iter):
         squared_diffs = tf.square(X[None, :, :] - centroids[:, None, :])
         euclidean_dists = tf.reduce_sum(squared_diffs, axis=-1) ** 0.5
-        clusters.assign(tf.argmin(euclidean_dists, axis=0))
+        clusters = tf.argmin(euclidean_dists, axis=0)
 
-        selector = tf.range(k)[:, None] == clusters[None, :]
-        
-        for c in tf.range(k):
+        selector = tf.range(k, dtype=tf.int64)[:, None] == clusters[None, :]
+        centroids_array = tf.TensorArray(tf.float32, size=k, element_shape=[nd, ])
+        mean_points = tf.zeros([nd, ], dtype=tf.float32)
+        for c in tf.range(k, dtype=tf.int64):
             select = selector[c]
             points = X[select]
             mean_points = tf.reduce_mean(points, axis=0)
-            centroids[c].assign(mean_points)
+            c = tf.cast(c, tf.int32)
+            centroids_array.write(c, mean_points)
+        centroids = centroids_array.stack()
     
     return centroids, clusters
+
+class KMeans:
+    @typechecked
+    def __init__(self, k: Union[int, tf.Tensor], 
+        n_iter: Union[int, tf.Tensor] = 10):
+        
+        self.k = k
+        self.n_iter = n_iter
+        self.centroids = None
+    
+    @typechecked
+    def fit_predict(self, X: Union[tf.Tensor, np.ndarray]) -> tf.Tensor:
+        self.centroids, clusters = kmeans(X, self.k, self.n_iter)
+        return clusters
+    
--- CSE2705/tf_kmeans_test.py
+++ CSE2705/tf_kmeans_test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-import numpy as np
 
-X = np.array([[ 2., 10.],
+X = tf.convert_to_tensor([
+    [ 2., 10.],
     [ 2.,  5.],
     [ 8.,  4.],
     [ 5.,  8.],
@@ -11,5 +11,12 @@ X = np.array([[ 2., 10.],
     [ 4.,  9.]])
 k = 3
 
+from tf_kmeans import KMeans
+
+kmeans = KMeans(k)
+clusters = kmeans.fit_predict(X)
+print(clusters)
+print(kmeans.centroids)
+
 import tf_kmeans
-tf_kmeans.train_kmeans(X, k)
+print(tf_kmeans.kmeans(X, k))
