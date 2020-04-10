fee882af687a5426ff3f64483a71001a2dbeb0b1
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-04-11 05:10:12 +0530

    Fix all bugs in TensorArray of tf_kmeans, working

--- CSE2705/tf_kmeans.py
+++ CSE2705/tf_kmeans.py
@@ -6,12 +6,12 @@ from typing import Union
 @tf.function
 def kmeans(X: Union[tf.Tensor, np.ndarray], 
     k: int, 
-    n_iter: int = 1) -> (tf.Tensor, tf.Tensor):
+    n_iter: int = 10) -> (tf.Tensor, tf.Tensor):
 
     X = tf.convert_to_tensor(X)
     X = tf.cast(X, tf.float32)
     assert len(tf.shape(X)) == 2, "Training data X must be represented as 2D array only"
-    m, nd = X.get_shape().as_list()
+    m = tf.shape(X)[0]
 
     random_select = tf.random.shuffle(X)
     init_centroids = random_select[:k]
@@ -25,23 +25,19 @@ def kmeans(X: Union[tf.Tensor, np.ndarray],
         clusters = tf.argmin(euclidean_dists, axis=0)
 
         selector = tf.range(k, dtype=tf.int64)[:, None] == clusters[None, :]
-        centroids_array = tf.TensorArray(tf.float32, size=k, element_shape=[nd, ])
-        mean_points = tf.zeros([nd, ], dtype=tf.float32)
-        for c in tf.range(k, dtype=tf.int64):
+        centroids_array = tf.TensorArray(tf.float32, size=k)
+        for c in tf.range(k):
             select = selector[c]
             points = X[select]
             mean_points = tf.reduce_mean(points, axis=0)
-            c = tf.cast(c, tf.int32)
-            centroids_array.write(c, mean_points)
+            centroids_array = centroids_array.write(c, mean_points)
         centroids = centroids_array.stack()
-    
+
     return centroids, clusters
 
 class KMeans:
     @typechecked
-    def __init__(self, k: Union[int, tf.Tensor], 
-        n_iter: Union[int, tf.Tensor] = 10):
-        
+    def __init__(self, k: int, n_iter: int = 10):
         self.k = k
         self.n_iter = n_iter
         self.centroids = None
--- CSE2705/tf_kmeans_test.py
+++ CSE2705/tf_kmeans_test.py
@@ -11,12 +11,14 @@ X = tf.convert_to_tensor([
     [ 4.,  9.]])
 k = 3
 
+import tf_kmeans
+print(tf.autograph.to_code(tf_kmeans.kmeans.python_function))
+
+print(tf_kmeans.kmeans(X, k))
+
 from tf_kmeans import KMeans
 
 kmeans = KMeans(k)
 clusters = kmeans.fit_predict(X)
 print(clusters)
 print(kmeans.centroids)
-
-import tf_kmeans
-print(tf_kmeans.kmeans(X, k))
